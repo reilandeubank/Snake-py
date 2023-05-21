c7ca98cd4e70bb92fb137ba67b246a40f16ead09
reilandeubank <reiland.eubank@yahoo.com>
2023-05-20 22:58:24 -0500

    Update agent.py
    stops improving at score of 35-40, need to give more inputs and figure out how to run on GPU.

--- agent.py
+++ agent.py
@@ -6,8 +6,8 @@ from snake import SnakeGame
 from model import Linear_QNet, QTrainer
 from helper import plot
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
+# print(torch.cuda.is_available())
+# print(torch.cuda.get_device_name(0))
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
