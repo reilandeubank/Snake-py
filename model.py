1da574296dad2f3da9c6a1744301eb22c7b9ba5f
reilandeubank <reiland.eubank@yahoo.com>
2023-05-21 22:02:03 -0500

    Gave Model more layers
    Record of 46

--- model.py
+++ model.py
@@ -9,11 +9,15 @@ class Linear_QNet(nn.Module):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, hidden_size)
+        self.linear4 = nn.Linear(hidden_size, hidden_size)
         self.linearOut = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
+        x = F.relu(self.linear4(x))
         x = self.linearOut(x)
         return x
 
